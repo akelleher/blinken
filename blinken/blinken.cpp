e76669f4c1d3455134c58acc0d723b6181ffcfbb
Alex Kelleher <alexpkelleher@gmail.com>
2016-04-25 18:46:33 -0400

    Add project files.

---
+++ blinken/blinken.cpp
@@ -0,0 +1,16 @@
+// blinken.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cstdlib>
+#include <iostream>
+#include <Windows.h>
+
+
+int main()
+{
+	std::cout << "Test" << std::endl;
+	getchar();
+    return 0;
+}
+
